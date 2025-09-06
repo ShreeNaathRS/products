301e9e4406487cfc3042a1f03d7db1a32f8a52f4
Shree Naath R S <shreenaath.rs@bounteous.com>
2025-09-06 21:38:13 +0530

    Removed unwanted code to trigger CI

--- src/main/java/com/swiftcart/products/constants/APIConstants.java
+++ src/main/java/com/swiftcart/products/constants/APIConstants.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class APIConstants {
-//	add new values added here to WebSecurityConfig
 	public static final List<String> ALLOWED_END_POINTS = new ArrayList<String>() {
 		{
 			add("/token");
